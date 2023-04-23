2ca6ac7cd4cc8fd6cbf193cb75fcab673b76b832
solarlime <solarlime@outlook.com>
2023-04-23 22:03:31 +0300

    Updated GitHub Actions

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: { port: 9001, hot: true },
   plugins: [
     new Dotenv(),
   ],
