9baa4254400ff9dda77fbef5efb543a89cd87b7f
solarlime <solarlime@outlook.com>
2024-12-30 23:40:39 +0300

    Now layout is more compact and readable on mobile devices

--- src/js/context.jsx
+++ src/js/context.jsx
@@ -1,12 +1,26 @@
-import { createContext, useOptimistic } from 'react';
+import { createContext, useOptimistic, useState, useEffect } from 'react';
 
 export const OptimisticContext = createContext(null);
 
 export const OptimisticProvider = ({ children, list }) => {
   const [optimisticList, setOptimisticList] = useOptimistic(list);
+  const [isCompact, setIsCompact] = useState(
+    document.documentElement.clientWidth <= 500,
+  );
+
+  useEffect(() => {
+    const compactMatcher = window.matchMedia('(max-width: 500px)');
+    const handleChange = (event) => {
+      setIsCompact(event.matches);
+    };
+    compactMatcher.addEventListener('change', handleChange);
+    return () => compactMatcher.removeEventListener('change', handleChange);
+  }, []);
 
   return (
-    <OptimisticContext.Provider value={{ optimisticList, setOptimisticList }}>
+    <OptimisticContext.Provider
+      value={{ optimisticList, setOptimisticList, isCompact }}
+    >
       {children}
     </OptimisticContext.Provider>
   );
