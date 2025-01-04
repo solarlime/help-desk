31b75b1c6521960f89a3b614d3037d3f48443f78
solarlime <solarlime@outlook.com>
2025-01-05 02:11:39 +0300

    Removed obsolete parts

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,24 +1,3 @@
-export default function validation(input) {
-  const message = input.nextElementSibling;
-  if (message) {
-    if (!input.validity.valid || !input.value.trim()) {
-      if (input.id !== 'title') {
-        message.classList.add('hidden');
-        return true;
-      }
-      if (input.validity.valueMissing) {
-        message.textContent = 'You need to fill the title in';
-      } else {
-        message.textContent = 'This value is invalid';
-      }
-      message.classList.remove('hidden');
-      return false;
-    }
-    message.classList.add('hidden');
-  }
-  return true;
-}
-
 const isValidName = (value) => /^[А-Яа-яËё. A-Za-z0-9]*$/.test(value);
 
 export { isValidName };
